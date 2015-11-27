ec5ede508b7817686f2fead9be410c27fc742b91
se7s199 <se7s199@gmail.com>
2015-11-27 13:01:19 +0200

    Update and rename gvusion.js to gvusion-2.js

--- gvusion.js
+++
@@ -1,9 +0,0 @@
-
-$(document)['ready'](function () {
-        $('#change2')['html']('<a href="http://blogger1dev.blogspot.com" rel="dofollow" target="_blank">مطور بلوجر</a>');
-        setInterval(function () {
-                if (!$('#change2:visible')['length']) {
-                  window['location']['href'] = 'http://blogger1dev.blogspot.com';
-                };
-            }, 3000);
-    });
